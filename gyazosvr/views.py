f48f3c016a1c5b464507770c6273d93d7b438e8f
den <den@wforest.jp>
2018-01-11 00:34:55 +0900

    コードを少しだけ綺麗にしたつもり。

--- gyazosvr/views.py
+++ gyazosvr/views.py
@@ -1,10 +1,13 @@
 import os
 import hashlib
+import logging
 from django.conf import settings
 from django.http import HttpResponse
 from django.utils import timezone
 from django.views import generic
 from django.views.defaults import bad_request
+
+log = logging.getLogger(__name__)
 
 
 class GyazoSvrIndexView(generic.TemplateView):
@@ -14,8 +17,7 @@ class GyazoSvrIndexView(generic.TemplateView):
 class Upload(generic.TemplateView):
 
     def get(self, request, *args, **kwargs):
-        return bad_request(request, Exception())
-        # return HttpResponse("POSTで送ってくんなましー")
+        return bad_request(request, Exception("not allowed GET request"))
 
     def post(self, request, *args, **kwargs):
         file_id = request.POST.get('id', None)
@@ -27,10 +29,18 @@ class Upload(generic.TemplateView):
         up_dir = os.path.join(settings.MEDIA_ROOT, 'up')
         if not os.path.exists(up_dir):
             os.mkdir(up_dir)
+
         file_name = file_id + '.png'
+
+        if 'imagedata' not in request.FILES:
+            return bad_request(request, Exception("no parameter: imagedata"))
+
         with open(os.path.join(up_dir, file_name), 'wb') as dest:
             for chunk in request.FILES['imagedata'].chunks():
                 dest.write(chunk)
 
         host = settings.GYAZOWINPSVR_HOST
-        return HttpResponse("http://{}/view/{}".format(host, file_name))
+        view_url = "http://{}/view/{}".format(host, file_name)
+        log.info("[UPLOADED] {}".format(view_url))
+
+        return HttpResponse(view_url)
